8715ae7b4b1f45ebe5b39f713f32f2de57f845d0
Vincent Tran <VincentTran197120@gmail.com>
2019-05-08 22:33:18 -0500

    Added custom property for components

--- src/app/Components/hello-comp/hello-comp.component.ts
+++ src/app/Components/hello-comp/hello-comp.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {Person} from '../../Interfaces/objectInt';
 @Component({
   selector: 'app-hello-comp',
   templateUrl: './hello-comp.component.html',
   styleUrls: ['./hello-comp.component.css']
 })
+
+
 export class HelloCompComponent implements OnInit {
   customerList:any[] = [{name: "stuff",id: 4,date: 4},{name: "stuff",id: 4,date: 4}]; //customerList = Name, Person type array
   customer = {
     name: "stuff"
   } 
+  @Input() get inputs():any[] { //inputs = property name
+    return this.customerList;
+  }
+  set inputs(list:any[]){
+    if(list){
+      console.log(list);
+      this.customerList = list;
+    }
+  }
+
   isVisible = true;
   constructor() { }
 
   ngOnInit() {
+  }
+  printout(){
+    console.log("Hello World");
   }
 
 }
